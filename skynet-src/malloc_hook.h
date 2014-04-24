9559c860e965aac80640f8dc9060dd88e4d70c76
Cloud Wu <cloudwu@gmail.com>
2014-04-24 17:55:43 +0800

    use pthread_getspecific instead of __thread

--- skynet-src/malloc_hook.h
+++ skynet-src/malloc_hook.h
@@ -3,12 +3,14 @@
 
 #include <stdlib.h>
 
-extern size_t malloc_used_memory(void);
-extern size_t malloc_memory_block(void);
-extern void   memory_info_dump(void);
-extern size_t mallctl_int64(const char* name, size_t* newval);
-extern int    mallctl_opt(const char* name, int* newval);
-extern void   dump_c_mem(void);
+size_t malloc_used_memory(void);
+size_t malloc_memory_block(void);
+void   memory_info_dump(void);
+size_t mallctl_int64(const char* name, size_t* newval);
+int    mallctl_opt(const char* name, int* newval);
+void   dump_c_mem(void);
+
+void malloc_inithook(void);
 
 #endif /* __MALLOC_HOOK_H */
 
